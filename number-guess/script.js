356da066d39409c7852c971049b65f37c4c1cb89
unknown <Katherine2bk@gmail.com>
2021-06-17 17:02:23 -0700

    spent three hours trying to figure out what was wrong with my input value, turns out I was trying to extract data from the button. fixed it, value is being displayed in console.

--- number-guess/script.js
+++ number-guess/script.js
@@ -1,1 +1,9 @@
 "use strict";
+
+// this is the check button
+const checkBtn = document.querySelector('.check-btn');
+// adding a click event to grab the input value
+checkBtn.addEventListener('click', () => {
+
+   console.log('clicked', document.querySelector('.check').value)
+})
