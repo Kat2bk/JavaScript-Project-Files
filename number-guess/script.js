6a4b78a08b06482dfc8e107576aed48f8bfd0ce1
unknown <Katherine2bk@gmail.com>
2021-06-17 17:35:53 -0700

    added an audio for negative input value, created a querySelector to grab the guess message... manipulated message depending on user input or no user input

--- number-guess/script.js
+++ number-guess/script.js
@@ -1,9 +1,28 @@
 "use strict";
 
+// sound effect for wrong input
+function nope() {
+    let audio = new Audio("death.mp3");
+    audio.play();
+}
+
+// need to compare this number to a randomly generated number
+
 // this is the check button
 const checkBtn = document.querySelector('.check-btn');
+// this is the message that displays for user input
+const message = document.querySelector('.guess');
+
 // adding a click event to grab the input value
 checkBtn.addEventListener('click', () => {
-
-   console.log('clicked', document.querySelector('.check').value)
+    // input will always be a string, need to convert to a number
+   const number = Number(document.querySelector('.check').value);
+   if (!number) {
+    // need to check if there is a value inside the input
+    nope();
+    message.textContent = "No number!"
+   } else if (number > 20) {
+       nope();
+       message.textContent = "Enter a valid number"
+   }
 })
