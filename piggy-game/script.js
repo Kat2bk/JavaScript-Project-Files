4ea8da4ee1ed444356e180b741b23158a34e1858
unknown <Katherine2bk@gmail.com>
2021-07-03 21:19:59 -0700

    file setup, will start on html

---
+++ piggy-game/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// user rolls dice, generate random roll
+// did user roll a one?
+// if no, add roll to score, display score
+// if yes, switch player
+// user holds score, add currant score to total
+// if score is greater than 100, user wins
+// if not, switch player
+// if user resets game, reset all score to 0
+// set user 1 as starting
