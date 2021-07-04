c6a5337fbea04485e670020e53a1bbf952a8e9f5
unknown <Katherine2bk@gmail.com>
2021-07-03 18:27:37 -0700

    mispelled a owrd in index.html - hide to hidden... grabbed elements with querySelector inside script.js

---
+++ modal-button/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// show modal button
+const showButton = document.querySelector('.show-modal');
+// button for exiting
+const exitButton = document.querySelector('.close-modal');
+// modal
+const modal = document.querySelector('.modal');
+// overlay
+const overlay = document.querySelector('.overlay');
+
