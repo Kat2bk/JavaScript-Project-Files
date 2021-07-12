345ca1edbed75af920a84e17ce4c269b2c185fa2
unknown <Katherine2bk@gmail.com>
2021-07-12 13:11:31 -0700

    grabbed necessary elements in script.js, configured dice roll for button click, along with sound

--- piggy-game/script.js
+++ piggy-game/script.js
@@ -1,5 +1,10 @@
 'use strict';
 
+function diceRolling() {
+    let audio = new Audio('./sounds/0004526.mp3');
+    audio.play();
+}
+
 // user rolls dice, generate random roll
 // did user roll a one?
 // if no, add roll to score, display score
@@ -13,4 +18,26 @@
 // score elements
 const scoreOne = document.querySelector('#score0');
 const scoreTwo = document.querySelector('#score1');
+
+// dice - hidden - starting condition
+const diceElement = document.querySelector('.dice');
+diceElement.classList.add('hidden');
+
+// button elements
+const btnNew = document.querySelector('.btn-new');
+const btnRoll = document.querySelector('.btn-roll');
+const btnHold = document.querySelector('.btn-hold');
+
+// rolling dice event 1 - 6
+btnRoll.addEventListener('click', () => {
+// generating random dice
+const dice = Math.trunc(Math.random() * 6) + 1;
+// display dice
+diceRolling();
+diceElement.classList.remove('hidden')
+diceElement.src = `./dice/dice-${dice}.png`
+// check for a one, if true, switch to next player
+})
+
+// computer rolling 1 - 6
 
