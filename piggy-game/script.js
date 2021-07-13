5270c1b387ebaabac52f685df3035f15a158bd4d
unknown <Katherine2bk@gmail.com>
2021-07-12 17:56:57 -0700

    finished function for computer roll, learned a lot with setTimeout and removing effects. now I need to know how to hold scores

--- piggy-game/script.js
+++ piggy-game/script.js
@@ -33,30 +33,100 @@ const scores = [0, 0];
 // dice - hidden - starting condition
 const diceElement = document.querySelector('.dice');
 diceElement.classList.add('hidden');
+// playerOne.classList.add('player-active');
 
 // button elements
 const btnNew = document.querySelector('.btn-new');
 const btnRoll = document.querySelector('.btn-roll');
 const btnHold = document.querySelector('.btn-hold');
-
-
-// rolling dice event 1 - 6
-btnRoll.addEventListener('click', () => {
-// generating random dice
-const dice = Math.trunc(Math.random() * 6) + 1;
-// display dice
-diceRolling();
-diceElement.classList.remove('hidden')
-diceElement.src = `./dice/dice-${dice}.png`
-// check for a one, if true, switch to next player
-if (dice !== 1) {
-    // add to current score
-    currentScore += dice;
-    currentOne.textContent = currentScore; // change later for active player
-} else {
-
-}
-})
+// btnRoll.disabled = false;
 
 // computer rolling 1 - 6
+// function computerRolling() {
+//     // let counter = 2;
+//     if (document.getElementById(`current${activePlayer}`) === 1) {
+//             diceRolling();
+//             console.log('dice roll player two', diceRoll)
+//             const diceRoll = Math.trunc(Math.random() * 6) + 1;
+//             diceElement.src = `./dice/dice-${diceRoll}.png` 
+//             if (diceRoll !== 1) {
+//                 currentScore += diceRoll;
+//                 document.getElementById(`current${activePlayer}`).textContent = currentScore;
+//                 if (currentScore < diceRoll) {
+//                     currentScore += diceRoll;
+//                     document.getElementById(`current${activePlayer}`).textContent = currentScore;
+//                 }
+//                 activePlayer = activePlayer === 1 ? 0 : 1;
+//                 playerTwo.classList.toggle('player-active');
+//                 playerOne.classList.toggle('player-active');
+//                 btnRoll.disabled = false;
+//                 btnRoll.classList.remove('disabledBtn');
+//             } else if (roll === 1) {
+//                 document.getElementById(`current${activePlayer}`).textContent = '☠️';
+//                 currentScore = 0;
+//                 activePlayer = activePlayer === 1 ? 0 : 1;
+//                 playerTwo.classList.toggle('player-active');
+//                 playerOne.classList.toggle('player-active');
+//                 btnRoll.disabled = false;
+//                 btnRoll.classList.remove('disabledBtn');
+//             }
+//         }  
+//     }
 
+    async function computerRolling() {
+        const count = [1, 2, 3];
+        setTimeout(() => {
+          for (let i = 0; i < count.length; i++) {
+              setTimeout(() => {
+                  diceRolling();
+                const diceRoll =  Math.trunc(Math.random() * 6) + 1;
+                diceElement.src = `./dice/dice-${diceRoll}.png` 
+            console.log('dice roll player two', diceRoll)
+            if (diceRoll !== 1) {
+                currentScore += diceRoll;
+                document.getElementById(`current${activePlayer}`).textContent = currentScore;
+                if (i == count.length - 1) {
+                    btnRoll.disabled = false;
+                    btnRoll.classList.remove('disabledBtn');
+                    activePlayer = activePlayer === 1 ? 0 : 1;
+                    playerTwo.classList.toggle('player-active');
+                    playerOne.classList.toggle('player-active');
+                }
+            } else if (diceRoll === 1) {
+                document.getElementById(`current${activePlayer}`).textContent = '☠️';
+                currentScore = 0;
+                btnRoll.disabled = false;
+                btnRoll.classList.remove('disabledBtn');
+                activePlayer = activePlayer === 1 ? 0 : 1;
+                playerTwo.classList.toggle('player-active');
+                playerOne.classList.toggle('player-active');
+            }
+              }, 1000 * i)
+          }     
+        }, 1000)
+    }
+
+    console.log('player two toggle', playerOne)
+    console.log('player two toggle', playerTwo)
+
+btnRoll.addEventListener('click', () => {
+    if (playerOne.className.includes('player-active')) {
+        const dice = Math.trunc(Math.random() * 6) + 1;
+        diceRolling();
+        diceElement.classList.remove('hidden')
+        diceElement.src = `./dice/dice-${dice}.png` 
+        if (dice !== 1) {
+            currentScore += dice;
+             document.getElementById(`current${activePlayer}`).textContent = currentScore;
+        } else {
+             document.getElementById(`current${activePlayer}`).textContent ='☠️';
+             currentScore = 0;
+             activePlayer = activePlayer === 0 ? 1 : 0;
+            playerOne.classList.toggle('player-active');
+            playerTwo.classList.toggle('player-active');
+            btnRoll.classList.add('disabledBtn');
+            btnRoll.disabled = true;
+            computerRolling();
+        }
+    }
+})
